954a0f9e1b739686f99a8ea9657bc16ecf04db34
jeanpierPol <je5x.1.jnes@gmail.com>
2024-05-25 13:33:56 +0200

    cambios en los nombres

--- routes/comprador.php
+++ routes/comprador.php
@@ -3,23 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('comprador')->name('comprador.')->group(function () {
-    Route::namespace('app\Http\Controllers\comprador')->group(function () {
+    Route::namespace('App\Http\Controllers\comprador')->group(function () {
         Route::middleware(['guest:comprador', 'revalidate'])->group(function () {
             Route::view('/login', 'comprador.auth.login')->name('login');
-            Route::post('/login', 'compradorController@loginHandler')->name('login-handler');
+            Route::post('/login', 'CompradorController@loginHandler')->name('login-handler');
 
             Route::view('/register', 'comprador.auth.register')->name('register');
-            Route::post('/register', 'compradorController@registerHandler')->name('register-handler');
+            Route::post('/register', 'CompradorController@registerHandler')->name('register-handler');
         });
 
         Route::middleware(['auth:comprador', 'revalidate'])->group(function () {
             Route::view('/home', 'comprador.home')->name('home');
-            Route::post('/logout', 'compradorController@logoutHandler')->name('logout');
+            Route::post('/logout', 'CompradorController@logoutHandler')->name('logout');
 
             Route::view('/profile', 'comprador.profile')->name('profile');
-            Route::get('/profile/update', 'compradorProfileController@showProfileUpdate')->name('profile.update');
-            Route::post('/profile/update', 'compradorProfileController@updateProfileHandler')->name('profile.update-handler');
-
+            Route::get('/profile/update', 'CompradorProfileController@showProfileUpdate')->name('profile.update');
+            Route::post('/profile/update', 'CompradorProfileController@updateProfileHandler')->name('profile.update-handler');
         });
     });
 });
+
