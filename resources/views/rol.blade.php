d6900b6ffbf75a9e3c6b5185c0d9eb3f263133df
jeanpierPol <je5x.1.jnes@gmail.com>
2024-05-29 17:31:45 +0200

    s

--- resources/views/rol.blade.php
+++ resources/views/rol.blade.php
@@ -13,15 +13,4 @@
       <div class="col-6 imagen_asignar_rol" style="background-image: url('/img/rol.png');">
       </div>
     </div>
-
-    <p>APP_KEY: {{ env.template('APP_KEY') }}</p>
-    <p>DB_HOST: {{ env.template('DB_HOST') }}</p>
-    <p>MAIL_MAILER: {{ env.template('MAIL_MAILER') }}</p>
-    <p>MAIL_HOST: {{ env.template('MAIL_HOST') }}</p>
-    <p>MAIL_PORT: {{ env.template('MAIL_PORT') }}</p>
-    <p>MAIL_USERNAME: {{ env.template('MAIL_USERNAME') }}</p>
-    <p>MAIL_PASSWORD: {{ env.template('MAIL_PASSWORD') }}</p>
-    <p>MAIL_ENCRYPTION: {{ env.template('MAIL_ENCRYPTION') }}</p>
-    <p>MAIL_FROM_ADDRESS: {{ env.template('MAIL_FROM_ADDRESS') }}</p>
-    <p>MAIL_FROM_NAME: {{ env.template('MAIL_FROM_NAME') }}</p>
 @endsection
